4807233e503d024b87c59a60677dacae0c2e2cfe
bornkes <born-kes@o2.pl>
2022-10-18 18:00:55 +0200

    Updates

--- static/js/main.4fa99cb2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfurniture-model3d-reactjs"]=this["webpackJsonpfurniture-model3d-reactjs"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var r=n(0),c=n(1),s=n.n(c),d=n(3),i=n.n(d);n(9);var j=function(){return Object(r.jsxs)("div",{children:[Object(r.jsx)("h1",{children:"hello Word!"}),Object(r.jsx)("nav",{})]})};i.a.render(Object(r.jsx)(s.a.StrictMode,{children:Object(r.jsx)(j,{})}),document.getElementById("view_model"))},9:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.4fa99cb2.chunk.js.map
