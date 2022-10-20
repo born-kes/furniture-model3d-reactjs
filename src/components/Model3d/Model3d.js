ce9cb73fbebd71b33176822081f2b32920bd7767
bornkes <born-kes@o2.pl>
2022-10-20 10:43:58 +0200

    stage setting

--- src/components/Model3d/Model3d.js
+++ src/components/Model3d/Model3d.js
@@ -1,37 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FigureBox from "../FigureBox/FigureBox";
-import './Model3d.css';
+import "./Model3d.css";
 
 const Model3d = () => {
-    const [element] = useState([
-        { id:1, className: 'sidewall left' },
-        { id:2, className: 'sidewall right' },
-        { id:3, className: 'capture top' },
-        { id:4, className: 'capture bottom' },
-        { id:5, className: 'shelves' }
-    ]);
-    const [dimensions] = useState({
-        x: '700',
-        y: '716',
-        z: '500',
-    });
-    const model3d = {
-        transform: 'rotateX(-20deg) rotateY(-20deg)',
-        transition: 'all 1s ease-in-out',
-        '--furniture--x': `${dimensions.x}px`,
-        '--furniture--y': `${dimensions.y}px`,
-        '--furniture--z': `${dimensions.z}px`
-    };
+  const [element] = useState([
+    { id: 1, className: "sidewall left" },
+    // { id:2, className: 'sidewall right' },
+    // { id:3, className: 'capture top' },
+    // { id:4, className: 'capture bottom' },
+    // { id:5, className: 'shelves' }
+  ]);
+  const [dimensions] = useState({
+    x: "700",
+    y: "300",
+    z: "500",
+    dagx: "-20deg",
+    dagy: "-20deg",
+  });
+  const model3d = {
+    transform: `rotateX(${dimensions.dagx}) rotateY(${dimensions.dagy}) scale(0.5)`,
+    transition: "all 1s ease-in-out",
+    "--furniture--x": `${dimensions.x}px`,
+    "--furniture--y": `${dimensions.y}px`,
+    "--furniture--z": `${dimensions.z}px`,
+  };
 
-    return (
-        <div  id="model3d" title="scene3d" style={model3d} >
-            <div className={'model3d-content furniture'}>
-                {
-                    element.map(({id, className})=><FigureBox key={id} className={className} />)
-                }
-            </div>
-        </div>
-    );
-}
+  return (
+    <div id="model3d" className="model3d" title="scene3d" style={model3d}>
+      <div className={"model3d-content x furniture"}>
+        {element.map(({ id, className }) => (
+          <FigureBox key={id} className={className} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Model3d;
