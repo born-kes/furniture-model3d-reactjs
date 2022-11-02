aa5a972b5aa01c3f0b6f0196a4e89de02de5d11a
bornkes <born-kes@o2.pl>
2022-11-02 11:02:12 +0100

    improve positioning transparency

--- src/components/FigureBox/FigureBox.js
+++ src/components/FigureBox/FigureBox.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./FigureBox.css";
 
-const FigureBox = ({className}) => {
+const FigureBox = ({className, style}) => {
     const _ClassName = ["figureBox", className].join(" ");
-
+    console.log(style)
+    const dimensions = {
+    "--figure--x": `${style.x}px`,
+    "--figure--y": `${style.y}px`,
+    "--figure--z": `${style.z}px`,
+    }
     return (
-            <div className={_ClassName}>
+            <div className={_ClassName} style={dimensions}>
                 <figure className={"front"}/>
                 <figure className={"back"}/>
                 <figure className={"top"}/>
--- src/components/Model3d/Model3d.js
+++ src/components/Model3d/Model3d.js
@@ -1,39 +1,39 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import FigureBox from "../FigureBox/FigureBox";
 import "./Model3d.css";
 
 const Model3d = () => {
-  const [element] = useState([
-    { id: 1, className: "sidewall left" },
-    // { id:2, className: 'sidewall right' },
-    // { id:3, className: 'capture top' },
-    // { id:4, className: 'capture bottom' },
-    // { id:5, className: 'shelves' }
-  ]);
   const [dimensions] = useState({
     x: "700",
-    y: "300",
-    z: "500",
+    y: "400",
+    z: "100",
     dagx: "-2deg",
     dagy: "-2deg",
   });
-  const model3d = {
-    transform: `rotateX(${dimensions.dagx}) rotateY(${dimensions.dagy}) `,
-    transition: "all 1s ease-in-out",
-    "--furniture--x": `${dimensions.x}px`,
-    "--furniture--y": `${dimensions.y}px`,
-    "--furniture--z": `${dimensions.z}px`,
-  };
+    const [element] = useState([
+        {id: 1, className: "sidewall left", style: {x: dimensions.x, y: dimensions.y, z: dimensions.z }},
+        // { id:2, className: 'sidewall right' },
+        // { id:3, className: 'capture top' },
+        // { id:4, className: 'capture bottom' },
+        // { id:5, className: 'shelves' }
+    ]);
+    const model3d = {
+        // transform: `rotateX(${dimensions.dagx}) rotateY(${dimensions.dagy}) `,
+        transition: "all 1s ease-in-out",
+        "--furniture--x": `${dimensions.x}px`,
+        "--furniture--y": `${dimensions.y}px`,
+        "--furniture--z": `${dimensions.z}px`,
+    };
 
-  return (
-    <div id="model3d" className="model3d" title="scene3d" style={model3d}>
-      <div className={"model3d-content x furniture"}>
-        {element.map(({ id, className }) => (
-          <FigureBox key={id} className={className} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="model3d" title="scene3d" style={model3d}>
+            <div className={"model3d-content x furniture"}>
+                {element.map(({id, className, style}) => (
+                    <FigureBox key={id} className={className} style={style}/>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Model3d;
